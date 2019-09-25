5d02f8bdf1f34f793e489f32e48be7437be11b12
xav921 <xav7470@gmail.com>
2019-09-25 09:03:05 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/Gauss.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive;
+
+public class Gauss {
+
+  public static void main(String[] args) {
+    int sum = 0;
+    for (int i = 1; i <= 100 ; i++) {
+      sum += i;
+    }
+    System.out.println(sum);
+  }
+}
